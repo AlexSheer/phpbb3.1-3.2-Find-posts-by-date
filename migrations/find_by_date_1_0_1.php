0e28537f9b58c7d1fcc810518c32bb1f47b77f74
AlexSheer <alex_sheer@inbox.lv>
2019-03-26 10:44:53 +0200

    Add files via upload

---
+++ migrations/find_by_date_1_0_1.php
@@ -0,0 +1,42 @@
+<?php
+/**
+*
+* @package phpBB Extension - Find posts & topics by date
+* @copyright (c) 2019 Sheer
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+namespace sheer\find_by_date\migrations;
+
+class find_by_date_1_0_1 extends \phpbb\db\migration\migration
+{
+	public function effectively_installed()
+	{
+		return isset($this->config['find_by_date_version']) && version_compare($this->config['find_by_date_version'], '1.0.1', '>=');
+	}
+
+	static public function depends_on()
+	{
+		return array('\sheer\find_by_date\migrations\find_by_date_1_0_0');
+	}
+
+	public function update_schema()
+	{
+		return array(
+		);
+	}
+
+	public function revert_schema()
+	{
+		return array(
+		);
+	}
+
+	public function update_data()
+	{
+		return array(
+			// Current version
+			array('config.update', array('find_by_date_version', '1.0.1')),
+		);
+	}
+}
